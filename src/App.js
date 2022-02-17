c9cb93a4aa4391024066238cdaffa7def7a6a9cc
PatrikG <patrik0gn@gmail.com>
2022-02-17 16:06:02 +0100

    Add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
+import About from './components/About.js';
+import Portfolio from './components/Portfolio.js';
+import Footer from './components/Footer.js';
 
 function App() {
-  return null;
+  return (
+    <>
+      <Header />
+      <main>
+        <About />
+        <Portfolio />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
