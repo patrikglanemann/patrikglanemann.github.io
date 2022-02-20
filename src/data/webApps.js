708ab2b2a0defd19477ef3b724bbac4d099b0fae
PatrikG <patrik0gn@gmail.com>
2022-02-20 21:12:56 +0100

    Add basic data structure

---
+++ src/data/webApps.js
@@ -0,0 +1,52 @@
+import readyevent from '../assets/webApps/readyevent-desktop.png';
+
+export const webApps = [
+  {
+    id: '01',
+    title: 'readyevent',
+    description:
+      'A event planer App for groups with Google Calendar integration.',
+    tech: [
+      'ReactJS',
+      'React Context API',
+      'Tailwind CSS',
+      'Material UI',
+      'Google API',
+    ],
+    gitHub: 'https://github.com/chingu-voyages/v34-geckos-team-04',
+    liveDemo: 'https://readyevent.herokuapp.com/',
+    image: readyevent,
+  },
+  {
+    id: '02',
+    title: 'readyevent',
+    description:
+      'A event planer App for groups with Google Calendar integration.',
+    tech: [
+      'ReactJS',
+      'React Context API',
+      'Tailwind CSS',
+      'Material UI',
+      'Google API',
+    ],
+    gitHub: 'URL',
+    liveDemo: 'URL',
+    image: 'URL',
+  },
+  {
+    id: '03',
+    title: 'readyevent',
+    description:
+      'A event planer App for groups with Google Calendar integration.',
+    tech: [
+      'ReactJS',
+      'React Context API',
+      'Tailwind CSS',
+      'Material UI',
+      'Google API',
+    ],
+    gitHub: 'URL',
+    liveDemo: 'URL',
+    image: 'URL',
+  },
+];
