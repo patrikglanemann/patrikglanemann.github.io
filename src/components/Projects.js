8338bf0103e1b44b680526842e2967730797931f
PatrikG <patrik0gn@gmail.com>
2022-02-21 18:56:28 +0100

    Add function to render Java Android projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import './Projects.css';
 import { webApps } from '../data/webApps';
+import { javaAndroid } from '../data/javaAndroid';
 import ProjectsItem from './ProjectsItem';
 
 export default function Projects() {
@@ -19,16 +20,32 @@ export default function Projects() {
     });
     return webAppsList;
   }
+  function renderJavaAndroid() {
+    const javaAndroidList = javaAndroid.map((javaAndroidProject) => {
+      return (
+        <ProjectsItem
+          key={javaAndroidProject.id}
+          title={javaAndroidProject.title}
+          description={javaAndroidProject.description}
+          gitHub={javaAndroidProject.gitHub}
+          image={javaAndroidProject.image}
+        />
+      );
+    });
+    return javaAndroidList;
+  }
 
   return (
-    <section className='Projects'>
+    <section id='projects' className='Projects'>
       <h2 className='Projects__title'>Projects</h2>
       <article>
         <h2 className='Projects__subTitle'>Web Apps</h2>
-        <div className='Projects__webApps'>{renderWebApps()}</div>
+        <div className='Projects__container'>{renderWebApps()}</div>
       </article>
-      <h2>Landingpages</h2>
-      <h2>Java Android Projects</h2>
+      <article>
+        <h2 className='Projects__subTitle'>Java Android Projects</h2>
+        <div className='Projects__container'>{renderJavaAndroid()}</div>
+      </article>
     </section>
   );
 }
