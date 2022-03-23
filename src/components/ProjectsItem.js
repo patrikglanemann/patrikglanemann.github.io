a394872b40f3adc9780ca648520c4de6fa78fc10
PatrikG <patrik0gn@gmail.com>
2022-03-23 12:02:39 +0100

    Change alt text

--- src/components/ProjectsItem.js
+++ src/components/ProjectsItem.js
@@ -1,4 +1,4 @@
-import './Projects.css';
+import "./Projects.css";
 
 export default function ProjectsItem({
   title,
@@ -9,31 +9,31 @@ export default function ProjectsItem({
   image,
 }) {
   return (
-    <div className='Project'>
+    <div className="Project">
       <div>
         {liveDemo ? (
           <a href={liveDemo}>
-            <img className='Project__image' src={image} alt='IMAGE'></img>
+            <img className="Project__image" src={image} alt="Project"></img>
           </a>
         ) : (
-          <img className='Project__image' src={image} alt='IMAGE'></img>
+          <img className="Project__image" src={image} alt="Project"></img>
         )}
       </div>
-      <div className='Project__info'>
-        <h3 className='Project__title'>{title}</h3>
+      <div className="Project__info">
+        <h3 className="Project__title">{title}</h3>
         <p>{description}</p>
         {tech ? (
-          <ul className='Project__tech'>
+          <ul className="Project__tech">
             {tech.map((item, index) => {
               return <li key={index}>{item} / </li>;
             })}
           </ul>
         ) : (
-          ''
+          ""
         )}
-        <div className='Project__links'>
+        <div className="Project__links">
           <a href={gitHub}>GitHub</a>
-          {liveDemo ? <a href={liveDemo}>Live Demo</a> : ''}
+          {liveDemo ? <a href={liveDemo}>Live Demo</a> : ""}
         </div>
       </div>
     </div>
