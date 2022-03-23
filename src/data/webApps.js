8a957e376ca8ec2e27e904437f05332db0b5fd09
PatrikG <patrik0gn@gmail.com>
2022-03-23 11:41:59 +0100

    Correct misspellings

--- src/data/webApps.js
+++ src/data/webApps.js
@@ -1,62 +1,62 @@
-import readyevent from '../assets/webApps/readyevent.png';
-import SudokusAndDragons from '../assets/webApps/SudokusAndDragons.png';
-import QuizApp from '../assets/webApps/QuizApp.png';
-import MTGCardSearch from '../assets/webApps/MTGCardSearch.png';
-import RickAndMorty from '../assets/webApps/RickAndMorty.png';
+import readyevent from "../assets/webApps/readyevent.png";
+import SudokusAndDragons from "../assets/webApps/SudokusAndDragons.png";
+import QuizApp from "../assets/webApps/QuizApp.png";
+import MTGCardSearch from "../assets/webApps/MTGCardSearch.png";
+import RickAndMorty from "../assets/webApps/RickAndMorty.png";
 
 export const webApps = [
   {
-    id: '1001',
-    title: 'readyevent (Teamproject)',
+    id: "1001",
+    title: "readyevent (Teamproject)",
     description:
-      'An event planer App for groups with Google Calendar integration.',
+      "An event planer App for groups with Google Calendar integration.",
     tech: [
-      'ReactJS',
-      'React Context API',
-      'Tailwind CSS',
-      'Material UI',
-      'Google API',
+      "ReactJS",
+      "React Context API",
+      "Tailwind CSS",
+      "Material UI",
+      "Google API",
     ],
-    gitHub: 'https://github.com/chingu-voyages/v34-geckos-team-04',
-    liveDemo: 'https://readyevent.herokuapp.com/',
+    gitHub: "https://github.com/chingu-voyages/v34-geckos-team-04",
+    liveDemo: "https://readyevent.herokuapp.com/",
     image: readyevent,
   },
   {
-    id: '1002',
-    title: 'Sudokus and Dragons',
+    id: "1002",
+    title: "Sudokus and Dragons",
     description:
-      'Sudoku with a room slection for difficulty and a highscore list.',
-    tech: ['ReactJS', 'React-Router', 'CSS', 'Local Storage', 'Sugoku API'],
-    gitHub: 'https://github.com/patrikglanemann/capstone-project',
-    liveDemo: 'https://capstone-project-sudokus-and-dragons.vercel.app/',
+      "Sudoku with a room selection for difficulty and a highscore list.",
+    tech: ["ReactJS", "React-Router", "CSS", "Local Storage", "Sugoku API"],
+    gitHub: "https://github.com/patrikglanemann/capstone-project",
+    liveDemo: "https://capstone-project-sudokus-and-dragons.vercel.app/",
     image: SudokusAndDragons,
   },
   {
-    id: '1003',
-    title: 'Quiz App',
-    description: 'Add your own questions and bookmark your favorite ones.',
-    tech: ['Vanilla JavaScript', 'Local Storage', 'HTML', 'CSS'],
-    gitHub: 'https://github.com/patrikglanemann/Quiz-App',
-    liveDemo: 'https://quiz-app-livid-nine.vercel.app/',
+    id: "1003",
+    title: "Quiz App",
+    description: "Add your own questions and bookmark your favorite ones.",
+    tech: ["Vanilla JavaScript", "Local Storage", "HTML", "CSS"],
+    gitHub: "https://github.com/patrikglanemann/Quiz-App",
+    liveDemo: "https://quiz-app-livid-nine.vercel.app/",
     image: QuizApp,
   },
   {
-    id: '1004',
-    title: 'MTG Card Search',
-    description: 'A simple card search for Magic the Gathering.',
-    tech: ['ReactJS', 'Local Storage', 'CSS', 'MtG API'],
+    id: "1004",
+    title: "MTG Card Search",
+    description: "A simple card search for Magic the Gathering.",
+    tech: ["ReactJS", "Local Storage", "CSS", "MtG API"],
     gitHub:
-      'https://github.com/patrikglanemann/MTG-react-app/tree/work_branch/',
-    liveDemo: 'https://mtg-react-app.vercel.app/',
+      "https://github.com/patrikglanemann/MTG-react-app/tree/work_branch/",
+    liveDemo: "https://mtg-react-app.vercel.app/",
     image: MTGCardSearch,
   },
   {
-    id: '1005',
-    title: 'Rick and Morty character search',
-    description: 'Search and filter characters from Rick and Morty.',
-    tech: ['ReactJS', 'CSS', 'Local Storage', 'RickAndMorty API'],
-    gitHub: 'https://github.com/patrikglanemann/-vite-vanilla-js-template-rnm',
-    liveDemo: 'https://vite-vanilla-js-template-rnm.vercel.app/',
+    id: "1005",
+    title: "Rick and Morty character search",
+    description: "Search and filter characters from Rick and Morty.",
+    tech: ["ReactJS", "CSS", "Local Storage", "RickAndMorty API"],
+    gitHub: "https://github.com/patrikglanemann/-vite-vanilla-js-template-rnm",
+    liveDemo: "https://vite-vanilla-js-template-rnm.vercel.app/",
     image: RickAndMorty,
   },
 ];
